c54e1215139f04cd40e6d888a9de6d7d95d70783
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-11-11 23:32:55 +0600

    explore condition type

--- index.ts
+++ index.ts
@@ -1,14 +1,47 @@
-// enum
+// // enum
 
-// type UserRole = 'Admin' | 'Editor'| 'Viewr';
+// // type UserRole = 'Admin' | 'Editor'| 'Viewr';
 
-// enum UserRole{
-//     Admin = 'Admin',
-//     Editor = 'Editor',
-//     Viewer = 'Viewer',
-// }
+// // enum UserRole{
+// //     Admin = 'Admin',
+// //     Editor = 'Editor',
+// //     Viewer = 'Viewer',
+// // }
 
-// const canEdit = (role: UserRole)=>{
+// // const canEdit = (role: UserRole)=>{
+// //     if(role === UserRole.Admin || role === UserRole.Editor){
+// //         return true;
+// //     }else return false;
+// // }
+
+// // const isEditPermissable = canEdit(UserRole.Admin)
+// // console.log(isEditPermissable)
+
+// // as const asssertion
+
+// // enum UserRole{
+// //     Admin = 'Admin',
+// //     Editor = 'Editor',
+// //     Viewer = 'Viewer',
+// // }
+
+// const UserRole = {
+//      Admin : 'Admin',
+//     Editor : 'Editor',
+//     Viewer : 'Viewer',
+// } as const;
+
+
+// // UserRole.Admin = 'Mon chai';
+
+
+// // const canEdit = (role:keyof typeof UserRole)=>{
+// //     if(role === UserRole.Admin || role === UserRole.Editor){
+// //         return true;
+// //     }else return false;
+// // }
+
+// const canEdit = (role: (typeof UserRole)[keyof typeof UserRole])=>{
 //     if(role === UserRole.Admin || role === UserRole.Editor){
 //         return true;
 //     }else return false;
@@ -17,35 +50,19 @@
 // const isEditPermissable = canEdit(UserRole.Admin)
 // console.log(isEditPermissable)
 
-// as const asssertion
+// conditional type
 
-// enum UserRole{
-//     Admin = 'Admin',
-//     Editor = 'Editor',
-//     Viewer = 'Viewer',
-// }
+type A = null
+type B = undefined
 
-const UserRole = {
-     Admin : 'Admin',
-    Editor : 'Editor',
-    Viewer : 'Viewer',
-} as const;
+type C = A extends null ? true : false;
 
-
-// UserRole.Admin = 'Mon chai';
-
-
-// const canEdit = (role:keyof typeof UserRole)=>{
-//     if(role === UserRole.Admin || role === UserRole.Editor){
-//         return true;
-//     }else return false;
-// }
-
-const canEdit = (role: (typeof UserRole)[keyof typeof UserRole])=>{
-    if(role === UserRole.Admin || role === UserRole.Editor){
-        return true;
-    }else return false;
+type BorolokerGari = {
+    bike: string,
+    car: string,
+    ship: string,
 }
 
-const isEditPermissable = canEdit(UserRole.Admin)
-console.log(isEditPermissable)
+type CheckGari<T> = T extends 'bike' | 'car' |'ship' ? true : false;
+
+type HasBike = CheckGari<'bike'>
