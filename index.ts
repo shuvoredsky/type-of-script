5d601d93104a6beca08c3bf94052938c4a0e63c1
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-11-11 23:11:55 +0600

    as const assertion explore

--- index.ts
+++ index.ts
@@ -2,13 +2,46 @@
 
 // type UserRole = 'Admin' | 'Editor'| 'Viewr';
 
-enum UserRole{
-    Admin = 'Admin',
-    Editor = 'Editor',
-    Viewer = 'Viewer',
-}
+// enum UserRole{
+//     Admin = 'Admin',
+//     Editor = 'Editor',
+//     Viewer = 'Viewer',
+// }
 
-const canEdit = (role: UserRole)=>{
+// const canEdit = (role: UserRole)=>{
+//     if(role === UserRole.Admin || role === UserRole.Editor){
+//         return true;
+//     }else return false;
+// }
+
+// const isEditPermissable = canEdit(UserRole.Admin)
+// console.log(isEditPermissable)
+
+// as const asssertion
+
+// enum UserRole{
+//     Admin = 'Admin',
+//     Editor = 'Editor',
+//     Viewer = 'Viewer',
+// }
+
+const UserRole = {
+     Admin : 'Admin',
+    Editor : 'Editor',
+    Viewer : 'Viewer',
+} as const;
+
+
+// UserRole.Admin = 'Mon chai';
+
+
+// const canEdit = (role:keyof typeof UserRole)=>{
+//     if(role === UserRole.Admin || role === UserRole.Editor){
+//         return true;
+//     }else return false;
+// }
+
+const canEdit = (role: (typeof UserRole)[keyof typeof UserRole])=>{
     if(role === UserRole.Admin || role === UserRole.Editor){
         return true;
     }else return false;
