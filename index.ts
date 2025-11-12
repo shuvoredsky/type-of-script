9b06abc67b64d2b29664254749c7744773056ed3
shuvo chakrabrati <kumarshuvo265@gmail.com>
2025-11-12 21:48:30 +0600

    explore mapped types

--- index.ts
+++ index.ts
@@ -52,17 +52,50 @@
 
 // conditional type
 
-type A = null
-type B = undefined
+// type A = null
+// type B = undefined
 
-type C = A extends null ? true : false;
+// type C = A extends null ? true : false;
 
-type BorolokerGari = {
-    bike: string,
-    car: string,
-    ship: string,
+// type BorolokerGari = {
+//     bike: string,
+//     car: string,
+//     ship: string,
+// }
+
+// type CheckGari<T> = T extends 'bike' | 'car' |'ship' ? true : false;
+
+// type HasBike = CheckGari<'bike'>
+
+const arrayOfNum: number[] = [1,4,6];
+
+const arrayOfStringUsingMap: string[] = arrayOfNum.map((num)=>num.toString())
+
+console.log(arrayOfStringUsingMap)
+
+const user = {
+    id: 222
 }
 
-type CheckGari<T> = T extends 'bike' | 'car' |'ship' ? true : false;
 
-type HasBike = CheckGari<'bike'>
+type AreaOfNum = {
+    height: number,
+    width: number,
+}
+
+type height = AreaOfNum['height']
+
+// type AreaOfString = {
+//    [key in 'height' | 'width']: string;
+// }
+
+type Area<T> = {
+    // [key in keyof T]: T[key];
+    [key in keyof T]: number;
+}
+
+const area1: Area<{height: string; width: string}> = {
+    height: 50,
+    width: 40,
+}
+
